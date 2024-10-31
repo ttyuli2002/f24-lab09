f519eff94e44c69abc989d1676d85302c4b19ddf
Qianting <gaoq4002@Gmail.com>
2024-10-31 19:07:53 -0400

    implemented mainAsync

--- src/index.ts
+++ src/index.ts
@@ -53,11 +53,34 @@ function main (fileNames: string[]): void {
     });
 }
 
+async function detectFaceAsync(fileName: string): Promise<void> {
+    console.log(`Running logo detection on ${fileName}`);
+    try {
+        const [result] = await client.logoDetection(fileName);
+        let scores: number[] = [];
+        const logos = result.logoAnnotations;
+        logos?.forEach((logo) => {
+            if (logo.description)
+                console.log(`"${logo.description}" found in in file ${fileName}`);
+            if (logo.score)
+                scores.push(logo.score);
+        });
+        const avg = scores.reduce((a, b) => a + b) / scores.length;
+        console.log(`Average score for ${fileName}: ${avg}`);        
+    } catch (err: any) {
+        if (err.code === 'ENOENT')
+            console.error(`File ${fileName} not found`);
+        else if (err.code == 7)
+            console.error(err.details);
+    }
+}
+
 // Implement the async version of the above here
 // Your version should not use .then and should use try/catch instead of .catch
 async function mainAsync(fileNames: string[]): Promise<void> {
-    console.error(new Error("mainAsync not implemented"));
-    // Your code here
+    for (const fileName of fileNames) {
+        await detectFaceAsync(fileName);
+    }
 }
 
 main([
